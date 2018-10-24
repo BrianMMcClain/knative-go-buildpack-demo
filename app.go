783c73d66a36d466d26b30363941600501bf010f
Brian McClain <brianmmcclain@gmail.com>
2018-10-24 12:22:01 -0400

    Update output

--- app.go
+++ app.go
@@ -8,7 +8,7 @@ import (
 
 func handlePost(rw http.ResponseWriter, req *http.Request) {
 	// Say Hello!
-	fmt.Fprintf(rw, "%s", "Hello from Go Buildpack!")
+	fmt.Fprintf(rw, "%s", "Hello from the Go Buildpack!")
 }
 
 func main() {
